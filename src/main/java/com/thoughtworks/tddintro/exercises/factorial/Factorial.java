43e2eddcffdb4e57c025f39a9c167093b88bf258
ledingto <edington.laura@gmail.com>
2017-07-18 22:03:46 -0700

    Test4 passes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,11 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i==2) {
-            return 2;
+        int factorial = 1;
+        for(int j=i; j>0; j--) {
+            factorial *= j;
         }
-        else {
-            return 1;
-        }
+        return factorial;
     }
 }
