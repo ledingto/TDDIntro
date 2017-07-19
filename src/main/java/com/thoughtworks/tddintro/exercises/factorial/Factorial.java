8e6f4dd37ab213df94134d01e1cfb5e5ca0141de
ledingto <edington.laura@gmail.com>
2017-07-18 22:01:13 -0700

    Test 2 (and 3) pass

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,11 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return 1;
+        if (i==2) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
     }
 }
