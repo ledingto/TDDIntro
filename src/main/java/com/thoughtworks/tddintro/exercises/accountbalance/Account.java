5c31aa8eeb4a43f5b054624b29f7fc160a56007a
ledingto <edington.laura@gmail.com>
2017-07-19 17:31:12 -0700

    Test1 passes (deposit functionality)

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+
+    private double balance;
+
+    public Account() {
+        balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,15 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        double origBalance = account.getBalance();
+        account.deposit(50);
+        assertTrue (account.getBalance() > origBalance);
     }
 
     @Test
