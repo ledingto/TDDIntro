5d39885cfc290112a3469621a234f6792e81d19b
ledingto <edington.laura@gmail.com>
2017-07-19 17:38:14 -0700

    Refactor with @Before to reduce duplication in tests

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,25 +1,30 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class AccountTests {
+    private Account account;
+    private double currBalance;
+    @Before
+    public void setUp() {
+        account = new Account();
+        currBalance = account.getBalance();
+    }
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
-        double origBalance = account.getBalance();
         account.deposit(50);
-        assertTrue (account.getBalance() > origBalance);
+        assertTrue (account.getBalance() > currBalance);
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account account = new Account();
-        double origBalance = account.getBalance();
         account.withdraw(50);
-        assertTrue(account.getBalance() < origBalance);
+        assertTrue(account.getBalance() < currBalance);
     }
 
     @Test
